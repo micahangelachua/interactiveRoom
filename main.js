fce598c0a48a45d72543566ec9c3e6663cd3cf97
Micah Angela Chua <cyprushervey23@gmail.com>
2022-10-21 20:33:21 +0800

    3rd

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+const secondHand = document.querySelector("[data-scnd]")
+const minuteHand = document.querySelector("[data-min]")
+const hourHand = document.querySelector("[data-hr]")
+
 function setClock() {
    const currentDate = new Date()
+   const seconds = currentDate.getSeconds() / 60
+   const minutes = (currentDate.getMinutes() + seconds) / 60
+   const hours = (currentDate.getHours() + minutes) / 12
 
-   console.log(currentDate);
+   console.log(currentDate)
+   console.log(seconds)
+   setRotation(secondHand, seconds)
+
+   console.log(minutes)
+   setRotation(minuteHand, minutes)
+
+   console.log(hours)
+   setRotation(hourHand, hours)
+}
+
+function setRotation(hand, angle) {
+   hand.style.setProperty('--rotation', angle * 360)
 }
 
 setInterval(setClock, 1000);
